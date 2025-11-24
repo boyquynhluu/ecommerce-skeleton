1515336a72943172316cc069ead45d952dc335f8
boyquynhluu <vanductai.dhv@gmail.com>
2025-11-24 22:47:23 +0700

    Init commit: add project structure and configs

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "ecommerce-skeleton"
+
+include("backend:gateway")
+include("backend:eureka-server")
+include("backend:catalog-service")
+include("backend:order-service")
+include("backend:inventory-service")
+include("backend:auth-service")
